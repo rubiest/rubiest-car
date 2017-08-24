68827588354d859225e2ebc76929e392a09e93da
AmirolAhmad <9w2pdf@gmail.com>
2017-08-24 14:33:22 +0800

    changes to custom devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
+  get "/dashboard" => 'dashboards#index'
+
   root 'pages#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    unlocks: 'users/unlocks'
+  }
+  devise_scope :user do
+    get '/login' => 'users/sessions#new'
+    get '/register' => 'users/registrations#new'
+    get '/settings' => 'users/registrations#edit'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
